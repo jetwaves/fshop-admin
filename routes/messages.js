31b68fac9d179fee762505dff590c1cedecff6f3
jetwaves <jetwaves@qq.com>
2015-02-13 11:59:01 +0800

    把微信群发消息的动能打了一个包方便外部调用
    Signed-off-by: jetwaves <jetwaves@qq.com>

--- routes/messages.js
+++ routes/messages.js
@@ -1,152 +1,3 @@
-var https = require('https');
-
-var appID = 'wx81465e521747011f';
-var appSecret = 'e3b8e0c47f91b4df0271c9e1fad85686';
-
-var appToken = '';
-
-var baseUrl = 'https://api.weixin.qq.com/';
-
-getTokenAction 		= 'cgi-bin/token?grant_type=client_credential&appid=' + appID + '&secret=' + appSecret;
-sendServiceMsgAction= '/cgi-bin/message/custom/send?access_token=';		// 因为这个url访问是post，因此最左边的/必不可少
-getFansListAction 	= 'cgi-bin/user/get?access_token=';
-
-var aSendRes = new Array();
-
-// 获取token
-function bulkSend(msg){
-	var fullUrl = baseUrl + getTokenAction; 	console.log(' full URL = ' + fullUrl);
-	https.get(baseUrl + getTokenAction, function(res){
-		console.log("statusCode: ", res.statusCode);
-		console.log("headers: ", res.headers);
-		res.on('data', function(body) {
-			// console.log(' res.onData  body: ');			console.dir(body);
-			parsedbody=JSON.parse(body);
-			console.log(' res.onData  parsed Body');	console.dir(parsedbody);
-			appToken = parsedbody.access_token;
-
-			sendMessageToGroup(msg);
-
-		});		
-	}).on('error', function(errMsg){
-		console.log(' https.onError ');		console.dir(errMsg)
-	});
-}
-
-
-
-// 循环群发消息
-function sendMessageToGroup(msg, nextOpenId ){
-	var getFansListURL = baseUrl + getFansListAction;
-	getFansListURL = getFansListURL + appToken;
-	// 获取用户列表	get方法，因此不同情况下path是不同的
-	if (nextOpenId  == '' || nextOpenId  == null) {
-		// 拉取第一批用户  getFansListURL 没有变化
-	} else{
-		// 用nextOpenId 做参数拉取下一批用户
-		getFansListURL = getFansListURL + '&next_openid=' + nextOpenId
-	};
-
-	https.get(getFansListURL, function(res){
-		res.on('data', function(body) {
-			// console.log(' sendMessageToGroup res.onData  body: ');			console.dir(body);
-			parsedbody=JSON.parse(body);
-			// console.log(' sendMessageToGroup res.onData  parsed Body');	console.dir(parsedbody);
-			total = parsedbody.total;
-			count = parsedbody.count;
-			data = parsedbody.data;
-			next_openid = parsedbody.next_openid;
-			console.log('	sendMessageToGroup 	body.total = ' + total);
-			console.log('	sendMessageToGroup 	body.count = ' + count);
-			console.log('	sendMessageToGroup 	body.next_openid = ' + next_openid);
-			if (next_openid != '') {
-				// 拉取下一组用户并发送消息
-				sendMessageToGroup(msg, next_openid);
-			} 
-			if ( count == 0) {
-				console.log('	sendMessageToGroup 	no more fans, exit loop');
-				return; 
-			};
-			console.log('  body.data'); console.dir(data);;
-			// oidList = data.openid;
-			// console.log('  oidList'); console.dir(oidList);
-			// 当前一组用户群发消息
-			for(userIdx in data.openid){
-				var uid = data.openid[userIdx];
-				console.log('	sendMessageToGroup 	loop   uid = ' + uid);
-				sendMessageToOpenId(msg, uid);
-			}
-		});		
-	}).on('error', function(errMsg){
-		console.log(' sendMessageToGroup.onError ');		console.dir(errMsg)
-	});
-
-}
-
-// 发送消息给一个用户
-function sendMessageToOpenId(msg, openId){
-	if (openId == '') { return; };
-	var optSendSingleMsg = {
-		hostname: 'api.weixin.qq.com',
-		port: 443,
-		path: sendServiceMsgAction + appToken,
-		method: 'POST',
-	};
-	var msgBody = 	{
-					    "touser": openId, 
-					    "msgtype": "text", 
-					    "text": {
-					        "content": msg
-					    }
-					};
-	var msgBody02 = {
-					    "touser": openId,
-					    "msgtype":"news",
-					    "news":{
-					        "articles": [
-					         {
-					             "title":"花果山水果特价活动",
-					             "description":"走过路过不要错过",
-					             "url":"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NjM4ODgyNA==&appmsgid=10000016&itemidx=1&sign=de8846e854ba25cc7298759d1cdc30b7#wechat_redirect",
-					             "picurl":"http://mmsns.qpic.cn/mmsns/Cv5dXkfOQoU3yX9aA5OibWWs6F0oC3T7K0AliciaJNEBaRgVOqiasM1BTA/0"
-					         },
-					         {
-					             "title":"好吃水果哪里强？",
-					             "description":"花果山上大哥强",
-					             "url":"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NjM4ODgyNA==&appmsgid=10000012&itemidx=1&sign=952fb473103ab93c84608aa657ccbf82#wechat_redirect",
-					             "picurl":"http://mmsns.qpic.cn/mmsns/Cv5dXkfOQoU3yX9aA5OibWWs6F0oC3T7KFZn4BskFlHSp9GaBOovm3w/0"
-					         }
-					         ]
-					    }
-					}
-
-	postData = JSON.stringify(msgBody);
-	/*console.log('	sendMessageToOpenId: Trying:   send text message to :' + openId);
-	console.log('   sendMessageToOpenId:   options = '); console.dir(optSendSingleMsg);*/
-	var req = https.request(optSendSingleMsg, function(res) {
-		res.on('data', function(body) {
-			parsedbody=JSON.parse(body);
-			console.dir(parsedbody);
-			if (parsedbody.errcode == 0 || parsedbody.errmsg == "ok") {
-				console.log('	sendMessageToOpenId' + openId + ': Success: ╰(*°▽°*)╯  text message sent');
-			} else {
-				console.log('	sendMessageToOpenId' + openId + ': fail:   	(┯_┯)		errmsg:' + parsedbody.errmsg);
-			}
-			aSendRes.push(parsedbody);
-		});
-	});
-	req.write(postData);
-	req.on('error', function(e) {
-		console.log(' sendMessageToOpenId.onError ');
-		console.error(e);
-	});
-	req.end();
-
-}
-
-//============================下面这一堆是网页相关的=====================================
-
-
 var express = require('express');
 var router = express.Router();
 
@@ -208,7 +59,6 @@ function sendPage(req, res){
 					msg      : "无", 
 				}
 	res.render("./" + modelName + "/msgsender.html", rend );
-
 }
 
 
@@ -220,21 +70,19 @@ function sendAction(req, res){
 	console.log('	messages.js 		sendAction 		app_secret = ' + app_secret);
 	console.log('	messages.js 		sendAction 		content = ' + content);
 
-	appID = app_id;
-	appSecret = app_secret;
-	getTokenAction 		= 'cgi-bin/token?grant_type=client_credential&appid=' + appID + '&secret=' + appSecret;
-	sendServiceMsgAction= '/cgi-bin/message/custom/send?access_token=';		// 因为这个url访问是post，因此最左边的/必不可少
-	getFansListAction 	= 'cgi-bin/user/get?access_token=';
+	var CMessenger = require('./wx_messenger.js');
+	var messenger = new CMessenger(app_id, app_secret);
+	messenger.bulkSend(content);
 
-	bulkSend(content);
 	setTimeout(function(){
 		var statsRes = {};
+		var aSendRes = messenger.aSendRes;
 		for(idx in aSendRes){
 			var item = aSendRes[idx];
 			console.log(' item = '); console.dir(item);
 			console.log(' statsRes[item.errcode] = '); console.dir(statsRes[item.errcode]);
 			if (statsRes[item.errcode] == null || statsRes[item.errcode] == undefined) {
-				 statsRes[item.errcode] = { code: item.errcode, msg: item.errmsg, count: 0};
+				 statsRes[item.errcode] = { code: item.errcode, msg: item.errmsg, count: 1};
 			} else{
 				console.log(' statsRes[item.errcode].count = '); console.dir(statsRes[item.errcode].count);
 				statsRes[item.errcode].count = eval(eval(statsRes[item.errcode].count) + 1);
@@ -246,7 +94,6 @@ function sendAction(req, res){
 		}
 		res.json(aStatsRes);
 	}, 5000);
-	// _infoJump(res, '发送成功，去看看自个手机吧' , '/messages/send');
 }
 
 function sendActionPROD(req, res){
@@ -286,10 +133,7 @@ function sendActionPROD(req, res){
 
 
 
-
-
-
-//=========================================================
+//==================普通CRUD================
 
 function _infoJump(res, info, targetUrl){
 	rend = { msg : info, targetUrl : targetUrl };
--- routes/wx_messenger.js
+++ routes/wx_messenger.js
@@ -1,38 +1,29 @@
 var https = require('https');
 
-var messenger = new Object();
-messenger.appID = '';
-messenger.appSecret = '';
-messenger.appToken = '';
-messenger.baseUrl = 'https://api.weixin.qq.com/';
-messenger.getTokenAction = 'cgi-bin/token?grant_type=client_credential&appid=' + this.appID + '&secret=' + this.appSecret;
-messenger.sendServiceMsgAction = '/cgi-bin/message/custom/send?access_token=';
-messenger.getFansListAction = 'cgi-bin/user/get?access_token=';
+function CMessenger(app_id, app_secret){
+	this.appID = app_id;
+	this.appSecret = app_secret;
+	this.appToken = '';
+	this.baseUrl = 'https://api.weixin.qq.com/';
+	this.getTokenAction = 'cgi-bin/token?grant_type=client_credential&appid=' + app_id + '&secret=' + app_secret;
+	this.sendServiceMsgAction = '/cgi-bin/message/custom/send?access_token=';
+	this.getFansListAction = 'cgi-bin/user/get?access_token=';
+	this.aSendRes = new Array();
+}
 
-messenger.createMessenger = function(app_id, app_secret){
-		this.baseUrl = 'https://api.weixin.qq.com/';
-		this.appID = app_id;
-		this.appSecret = app_secret;
-		this.getTokenAction = 'cgi-bin/token?grant_type=client_credential&appid=' + this.appID + '&secret=' + this.appSecret;
-		this.sendServiceMsgAction = '/cgi-bin/message/custom/send?access_token=';
-		this.getFansListAction = 'cgi-bin/user/get?access_token=';
-		return this;
-};
-
-messenger.bulkSend = function(msg)
-{
-	// 获取token
+CMessenger.prototype.bulkSend = function(msg) {
+	// 获取token	并群发
 	var fullUrl = this.baseUrl + this.getTokenAction; 	console.log(' full URL = ' + fullUrl);
-	var _messenger = this;
-	https.get(_messenger.baseUrl + _messenger.getTokenAction, function(res){
-		console.log("statusCode: ", res.statusCode);
-		console.log("headers: ", res.headers);
-		res.on('data', function(body) {
-			// console.log(' res.onData  body: ');			console.dir(body);
+	var rMessenger = this;
+	https.get(fullUrl, function(httpRes){
+		// console.log("statusCode: ", httpRes.statusCode);		console.log("headers: ", httpRes.headers);
+		httpRes.on('data', function(body) {
+			// console.log(' httpRes.onData  body: ');			console.dir(body);
 			parsedbody=JSON.parse(body);
-			console.log(' res.onData  parsed Body');	console.dir(parsedbody);
-			messenger.appToken = parsedbody.access_token;
-			messenger.sendMessageToGroup(msg, '');
+			console.log(' httpRes.onData   rMessenger : ');		console.dir(rMessenger);
+			console.log(' httpRes.onData  parsed Body');	console.dir(parsedbody);
+			rMessenger.appToken = parsedbody.access_token;
+			rMessenger.sendMessageToGroup(msg, '');
 
 		});		
 	}).on('error', function(errMsg){
@@ -40,8 +31,56 @@ messenger.bulkSend = function(msg)
 	});
 };
 
-messenger.sendMessageToGroup = function (msg, nextOpenId )
-{
+CMessenger.prototype.sendMessageToOpenId = function(msg, openId) {
+	var rMessenger = this;
+	// 发送消息给一个用户
+	if (openId == '') { return; };
+	var optSendSingleMsg = {
+		hostname: 'api.weixin.qq.com',
+		port: 443,
+		path: this.sendServiceMsgAction + this.appToken,
+		method: 'POST',
+	};
+	var msgBody = 	{
+					    "touser": openId, 	    "msgtype": "text", 
+					    "text": {	"content": msg    }
+					};
+/*	var msgBody02 = {		// 图文消息样本
+					    "touser": openId,		"msgtype":"news",
+					    "news":{
+					        "articles": [{
+					             "title":"花果山水果特价活动",
+					             "description":"走过路过不要错过",
+					             "url":"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NjM4ODgyNA==&appmsgid=10000016&itemidx=1&sign=de8846e854ba25cc7298759d1cdc30b7#wechat_redirect",
+					             "picurl":"http://mmsns.qpic.cn/mmsns/Cv5dXkfOQoU3yX9aA5OibWWs6F0oC3T7K0AliciaJNEBaRgVOqiasM1BTA/0"
+					         },{
+					             "title":"好吃水果哪里强？",
+					             "description":"花果山上大哥强",
+					             "url":"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NjM4ODgyNA==&appmsgid=10000012&itemidx=1&sign=952fb473103ab93c84608aa657ccbf82#wechat_redirect",
+					             "picurl":"http://mmsns.qpic.cn/mmsns/Cv5dXkfOQoU3yX9aA5OibWWs6F0oC3T7KFZn4BskFlHSp9GaBOovm3w/0"
+					         }]
+					    }
+					}*/
+	postData = JSON.stringify(msgBody);
+	/*console.log('	sendMessageToOpenId: Trying:   send text message to :' + openId);
+	console.log('   sendMessageToOpenId:   options = '); console.dir(optSendSingleMsg);*/
+	var req = https.request(optSendSingleMsg, function(httpRes) {
+		httpRes.on('data', function(data) {
+			console.log('	sendMessageToOpenId: Success:   text message sent to :' + openId);
+			data = JSON.parse(data);
+			// console.log('   sendMessageToOpenId: data = '); 	console.dir(data);
+			rMessenger.aSendRes.push(data);
+		});
+	});
+	req.write(postData);
+	req.on('error', function(e) {
+		console.log(' sendMessageToOpenId.onError ');
+		console.error(e);
+	});
+	req.end();
+};
+
+CMessenger.prototype.sendMessageToGroup = function(msg, nextOpenId) {
 	// 循环群发消息
 	var getFansListURL = this.baseUrl + this.getFansListAction;
 	getFansListURL = getFansListURL + this.appToken;
@@ -52,12 +91,12 @@ messenger.sendMessageToGroup = function (msg, nextOpenId )
 		// 用nextOpenId 做参数拉取下一批用户
 		getFansListURL = getFansListURL + '&next_openid=' + nextOpenId
 	};
-
-	https.get(getFansListURL, function(res){
-		res.on('data', function(body) {
-			// console.log(' sendMessageToGroup res.onData  body: ');			console.dir(body);
+	var rMessenger = this;
+	https.get(getFansListURL, function(httpRes){
+		httpRes.on('data', function(body) {
+			// console.log(' sendMessageToGroup httpRes.onData  body: ');			console.dir(body);
 			parsedbody=JSON.parse(body);
-			// console.log(' sendMessageToGroup res.onData  parsed Body');	console.dir(parsedbody);
+			// console.log(' sendMessageToGroup httpRes.onData  parsed Body');	console.dir(parsedbody);
 			total = parsedbody.total;
 			count = parsedbody.count;
 			data = parsedbody.data;
@@ -67,84 +106,28 @@ messenger.sendMessageToGroup = function (msg, nextOpenId )
 			console.log('	sendMessageToGroup 	body.next_openid = ' + next_openid);
 			if (next_openid != '') {
 				// 拉取下一组用户并发送消息
-				this.sendMessageToGroup(msg, next_openid);
+				rMessenger.sendMessageToGroup(msg, next_openid);
 			} 
 			if ( count == 0) {
 				console.log('	sendMessageToGroup 	no more fans, exit loop');
 				return; 
 			};
 			console.log('  body.data'); console.dir(data);;
-			// oidList = data.openid;
-			// console.log('  oidList'); console.dir(oidList);
 			// 当前一组用户群发消息
 			for(userIdx in data.openid){
 				var uid = data.openid[userIdx];
 				console.log('	sendMessageToGroup 	loop   uid = ' + uid);
-				this.sendMessageToOpenId(msg, uid);
+				rMessenger.sendMessageToOpenId(msg, uid);
 			}
 		});		
 	}).on('error', function(errMsg){
 		console.log(' sendMessageToGroup.onError ');		console.dir(errMsg)
 	});
-
-};
-messenger.sendMessageToOpenId = function (msg, openId)
-{
-	// 发送消息给一个用户
-	if (openId == '') { return; };
-	var optSendSingleMsg = {
-		hostname: 'api.weixin.qq.com',
-		port: 443,
-		path: this.sendServiceMsgAction + this.appToken,
-		method: 'POST',
-	};
-	var msgBody = 	{
-					    "touser": openId, 
-					    "msgtype": "text", 
-					    "text": {
-					        "content": msg
-					    }
-					};
-	var msgBody02 = {
-					    "touser": openId,
-					    "msgtype":"news",
-					    "news":{
-					        "articles": [
-					         {
-					             "title":"花果山水果特价活动",
-					             "description":"走过路过不要错过",
-					             "url":"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NjM4ODgyNA==&appmsgid=10000016&itemidx=1&sign=de8846e854ba25cc7298759d1cdc30b7#wechat_redirect",
-					             "picurl":"http://mmsns.qpic.cn/mmsns/Cv5dXkfOQoU3yX9aA5OibWWs6F0oC3T7K0AliciaJNEBaRgVOqiasM1BTA/0"
-					         },
-					         {
-					             "title":"好吃水果哪里强？",
-					             "description":"花果山上大哥强",
-					             "url":"http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NjM4ODgyNA==&appmsgid=10000012&itemidx=1&sign=952fb473103ab93c84608aa657ccbf82#wechat_redirect",
-					             "picurl":"http://mmsns.qpic.cn/mmsns/Cv5dXkfOQoU3yX9aA5OibWWs6F0oC3T7KFZn4BskFlHSp9GaBOovm3w/0"
-					         }
-					         ]
-					    }
-					}
-
-	postData = JSON.stringify(msgBody02);
-	/*console.log('	sendMessageToOpenId: Trying:   send text message to :' + openId);
-	console.log('   sendMessageToOpenId:   options = '); console.dir(optSendSingleMsg);*/
-	var req = https.request(optSendSingleMsg, function(res) {
-		res.on('data', function(res) {
-			console.log('	sendMessageToOpenId: Success:   text message sent to :' + openId);
-		});
-	});
-	req.write(postData);
-	req.on('error', function(e) {
-		console.log(' sendMessageToOpenId.onError ');
-		console.error(e);
-	});
-	req.end();
-
 };
 
 
+CMessenger.prototype.haha = function(msg) {
+	console.log(' CMessenger.haha()    ' + msg);
+};
 
-module.exports = messenger;
-
-
+module.exports = CMessenger;
